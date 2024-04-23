62ba1ad20a2359e528d633c90b68c156e3276060
sulfikaralijun <sulfikaralijun@gmail.com>
2024-04-23 16:05:53 +0800

    first commit

---
+++ src/components/Result.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import { useCounterStore }from '../stores/counter.js'
+  const count = useCounterStore()
+</script>
+
+<template>
+  <div>
+    <p>Double: {{ count.doubleCount }}</p>
+  
+  </div>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { useCounterStore }from '../stores/counter.js'
+  import Result from "../components/Result.vue"
+  const count = useCounterStore()
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ count.count }}</p>
+    <button @click="count.increment()">Single</button>
+    <Result/>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
